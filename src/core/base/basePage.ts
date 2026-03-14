dd4a4dfab08a5cf2edfe61f55678a04cba3a371b
Ash-leyPereira <130834938+Ash-leyPereira@users.noreply.github.com>
2026-03-14 11:55:27 +0530

    feat(core): add BasePage class for reusable UI actions
    file added-
    src/core/base/BasePage.ts

---
+++ src/core/base/basePage.ts
@@ -0,0 +1,19 @@
+import { Page } from "@playwright/test";
+
+export class BasePage {
+
+ constructor(protected page: Page) {}
+
+ async navigate(url: string) {
+   await this.page.goto(url);
+ }
+
+ async click(locator: string) {
+   await this.page.locator(locator).click();
+ }
+
+ async fill(locator: string, value: string) {
+   await this.page.fill(locator, value);
+ }
+
+}
